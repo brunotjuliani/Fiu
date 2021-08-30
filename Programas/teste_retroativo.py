fb3b893030b2d00b4c1e01b38b26de07fcd59cfd
brunotjuliani <brunotjuliani@hotmail.com>
2021-08-30 14:38:54 -0300

    rodada 27-08

--- Programas/teste_retroativo.py
+++ Programas/teste_retroativo.py
@@ -6,7 +6,7 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 # Leitura
-dispara = dt.datetime(2021, 8, 13,  00, tzinfo=dt.timezone.utc)
+dispara = dt.datetime(2021, 8, 20,  00, tzinfo=dt.timezone.utc)
 d_prev = dispara.isoformat()
 ini_obs = dispara-dt.timedelta(days=5)
 ini_obs = ini_obs.isoformat()
